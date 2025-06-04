454b31c074c40df653ba37bae6fb0be1dba9fe69
Théo <theo.cartegnie@le-campus-numerique.fr>
2025-06-04 17:03:12 +0200

    Fix "About us" link on footer

--- Templates/footer.php
+++ Templates/footer.php
@@ -2,7 +2,7 @@
    <footer>
             <img class = "logo" src = "images/icons8-enclume-100.png" alt = "Picture of an enclume with a hammer on it, its the logo of the entreprise">
             <h1> COMICS LIBRARY </h1>
-            <a class = "footerLink" href = "AboutUs.html"> About us</a>
+            <a class = "footerLink" href = "AboutUs.php"> About us</a>
     </footer>
 </body>
 
